a2886259a7e6f54b3d4558b6210fd5ed161fe357
arthur <arthur.rios007@gmail.com>
2024-05-31 14:04:19 -0300

    feat: Add user component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import { colors } from './src/styles/colors'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
